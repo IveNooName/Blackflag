9f335f4994dae9f5385954903cba6b96b30f948a
IveNooName <schaed.fab@gmail.com>
2026-05-26 08:19:41 +0200

    Initial commit

---
+++ src/test/java/com/ivenooname/blackflag/BlackflagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivenooname.blackflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackflagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
