dd92d14159a8c1d13d0b40f97b03755cb598b28d
IveNooName <schaed.fab@gmail.com>
2026-05-30 00:32:37 +0200

    almost finished Backend. (javelit is missing)

---
+++ src/main/java/com/ivenooname/blackflag/exception/ContainerErrorCodeException.java
@@ -0,0 +1,7 @@
+package com.ivenooname.blackflag.exception;
+
+public class ContainerErrorCodeException extends RuntimeException {
+	public ContainerErrorCodeException(String message) {
+		super(message);
+	}
+}
